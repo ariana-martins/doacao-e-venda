c3f68b78d7124b75aa1ffeac099fca7d62dfdc2d
Ariana <arianaestrelar@gmail.com>
2024-02-29 15:27:26 -0300

    ...

--- src/pages/MeusProdutosTopTab/index.js
+++ src/pages/MeusProdutosTopTab/index.js
@@ -23,73 +23,119 @@ export default function MeusProdutos() {
    
     const [data, setData] = useState('');
 
-// Refazer o getDowload em "Adicionar novo produto" e aqui também incluir um "UseEffect + getDetail"
-// Conforme seguindo o manual do canal do Youtube: CODERS NEVER QUIT
-// Título do vídeo: Part 1/2 | OLX Clone using React Native & Firebase | React Native & Firebase for beginners in Hindi
-// Link canal do Youtube: https://www.youtube.com/watch?v=ntPQ-IPm3AM&list=PLB97yPrFwo5ihgCoWXlEDHrAPQNshsfzP&index=7
-// Observação: Nesse exemplo inicia desde o login com o usuário, e mostra o "produto c/ imagem" em um Card c/ getDowload do Firebase + Storage.
-const ref = firebase.firestore().collection('produtos');
-    useEffect(()=>{
-        ref.onSnapshot(querySnapshot =>{
-            const data = []
-            querySnapshot.forEach(doc =>{
-                data.push({
-                     ...doc.data(),
-                        key:doc.id
-                })
+
+
+//a partir de então 
+useEffect(()=>{
+    //where p/ filtrar meus produtos, só vai mostrar as publicações do usuário logado,
+    // e dentro do where, irá os parametros de quem é usuario ("id do usuário", "igual", usuário) 
+    let ref = firebase.firestore().collection('produtos').where('user_id', '==', user_id)
+        .onSnapshot(querySnapshot =>{
+        const data = []
+        querySnapshot.forEach(doc =>{
+            data.push({ //eu quero que o banco de dados traga todas as informações 
+                 ...doc.data(),
+                    key:doc.id // do banco que esteja este usuario que está logado
             })
-                setData(data)
         })
-      //  return () => ref()
-    }, [])
-
-    
-
-    //Para deletar e/ou editar apenas os produtos que um usuário adicionou, e não deletar todos os produtos de todos os usuários
-    // Ou seja, vai fazer um filtro para filtrar somente os produtos do usuário "x".
-    const user_id = firebase.auth().currentUser.uid;
+            setData(data)
+    })
+    return () => ref()
+}, [])
 
 
-    return (
-        <View style={{ padding: 18, backgroundColor: '#FFFFFF' }}>
-            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Meus produtos para doar e vender!</Text>
-       
-                <FlatList
-                    data={data}
-                    renderItem={({item}) => (
-                        <View style={{ marginTop: 14 }}>
-                            <Card containerStyle={{ marginTop: 15 }}
-                               
-                            //   key={item.id} //uma id para cada produto
-                            >
-                                <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{user_id}</Text> 
-                                {/*aqui vai o card de imagens, mas não está puxando do banco de dados todas as imagens*/}
-                                <Card.Image 
-                                    style={styles.img}
-                                    source={{uri : item.images}} 
-                                />
-                                <Card.Divider />
-                                <Card.Title>{item.titulo}</Card.Title>
-                                <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{item.valor}</Text> 
-                                
-                                <Text style={{ fontSize: 16, marginTop: 5 }}>{item.descricao}</Text>
-                                
-                            </Card>
-                        </View>
-                    )}
-                />
 
-        </View>
-    );
 
-}
+  //variável constante
+  const user_id = firebase.auth().currentUser.uid
+
+return (
+
+    <View style={{ padding: 18, backgroundColor: '#FFFFFF' }}>
+    <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Meus produtos para doar e vender!</Text>
+
+  
+            <FlatList
+                data={data}
+                renderItem={({ item }) => (
+                    <View style={{ marginTop: 14 }}>
+                        <Card containerStyle={{ marginTop: 15 }}>
+                            <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{user_id}</Text>
+                            {/*aqui vai o card de imagens, mas não está puxando do banco de dados todas as imagens*/}
+                            <Card.Image
+                                style={styles.img}
+                                source={{ uri: item.images }}
+                            />
+                            <Card.Divider />
+                            <Card.Title>{item.titulo}</Card.Title>
+                            <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{item.valor}</Text>
+
+                            <Text style={{ fontSize: 16, marginTop: 5 }}>{item.descricao}</Text>
+
+                        </Card>
+                       
+                    </View>
+                )}
+            />
+        
+
+    </View >
+
+
+
+
+
+    /* <View
+        style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center", //centralizando todos os textos e imagens ao centro da tela (no meio da tela em geral)
+            backgroundColor: "#FFFFFF",
+        }}
+    >
+        <Text
+            style={{
+                fontSize: 20,
+                color: "#000000",
+                fontWeight: "800"
+            }}
+        >
+            Interesses está aqui
+        </Text>
+    </View>
+*/
+
+);
+};
 
 
 
 const styles = StyleSheet.create({
-    img: {   
-        padding: 0,
-        resizeMode: 'contain',
-    },
+container: {
+    flex: 1,
+    padding: 25,
+    backgroundColor: 'white',
+},
+textInputStyle: {
+    width: '100%',
+    height: 40,
+    paddingHorizontal: 5,
+    borderWidth: 0.5,
+    marginTop: 20,
+},
+button: {
+    width: 150,
+    height: 50,
+    borderRadius: 10,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center', //se utilizar "center" //justifica todos os textos e imagens ao centro da tela (exemplo: centralizado na lateral esquerda da tela)
+    alignItems: 'center', //centralizando todos os textos e imagens ao centro da tela (no meio da tela em geral)
+},
+img: {   
+    padding: 0,
+    resizeMode: 'contain',
+},
 });
+
+
 

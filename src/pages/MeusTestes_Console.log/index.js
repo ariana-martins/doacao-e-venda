980541a67efe37884292b98f54e6f8c12cf8d6dd
Ariana <arianaestrelar@gmail.com>
2024-10-22 09:43:59 -0300

    Comentários exemplo de Estilo de Login

--- src/pages/MeusTestes_Console.log/index.js
+++ src/pages/MeusTestes_Console.log/index.js
@@ -405,3 +405,76 @@ const styles = StyleSheet.create({
 });
 
 */
+
+/*
+// =========== Exemplo de Estilo para a página do Login ==========================
+
+import React from "react";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import { windowHeight, windowWidth } from "../../utils/Dimensions";
+
+export default function FormButton({ buttonTitle, ...rest }) {
+    return (
+        <TouchableOpacity style={styles.buttonContainer} {...rest}>
+            <Text style={styles.buttonText}>{buttonTitle}</Text>
+        </TouchableOpacity>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        marginTop: 10,
+        width: windowWidth / 2,
+        height: windowHeight / 15,
+        backgroundColor: '#6646ee',
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8,
+    },
+    buttonText: {
+        fontSize: 28,
+        color: '#FFFFFF',
+    },
+})
+
+*/
+// ================================================================
+
+/*
+// =========== Exemplo de Estilo para a página do Login ==========================
+
+
+import React from "react";
+import { StyleSheet, TextInput } from "react-native";
+import { windowHeight, windowWidth } from "../utils/Dimensions";
+
+export default function FormInput({ labelValue, placeholderText, ...rest }) {
+    return (
+        <TextInput
+            value={labelValue}
+            style={styles.input}
+            numberOfLines={1}
+            placeholder={placeholderText}
+            placeholderTextColor='#666'
+            {...rest}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        padding: 10,
+        marginTop: 5,
+        marginBottom: 10,
+        width: windowWidth / 1.5,
+        height: windowHeight / 15,
+        fontSize: 16,
+        borderRadius: 8,
+        borderWidth: 1,
+    },
+})
+
+*/
+// ============================================================================
